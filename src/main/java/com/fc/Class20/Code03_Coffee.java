7df00d2e2b5ac57add327881c2b4852ef5ab8c6b
Fc_Clb <lexiangjizhi@qq.com>
2026-04-11 16:37:00 +0800

    咖啡机问题（包含两问，所有人喝上咖啡的最短时间，杯子都干净的最短时间）

--- src/main/java/com/fc/Class20/Code03_Coffee.java
+++ src/main/java/com/fc/Class20/Code03_Coffee.java
@@ -5,9 +5,12 @@ import java.util.PriorityQueue;
 
 public class Code03_Coffee {
 
+    //问题一：每个人都喝上咖啡的最短时间
+
+    //咖啡机
     public static class Machine {
-        public int timePoint;
-        public int workTime;
+        public int timePoint;   //可以工作的时间
+        public int workTime;    //泡咖啡所用时间
 
         public Machine(int timePoint, int workTime) {
             this.timePoint = timePoint;
@@ -15,6 +18,7 @@ public class Code03_Coffee {
         }
     }
 
+    //自定义比较器
     public static class MachineComparator implements Comparator<Machine> {
 
         @Override
@@ -23,7 +27,7 @@ public class Code03_Coffee {
         }
     }
 
-    public static int minTIme2(int[] arr, int n, int a, int b) {
+    public static int minTIme1(int[] arr, int n, int a, int b) {
         PriorityQueue<Machine> heap = new PriorityQueue<Machine>(new MachineComparator());
         for (int i = 0; i < n; i++) {
             heap.add(new Machine(0, arr[i]));
@@ -36,11 +40,71 @@ public class Code03_Coffee {
             heap.add(cur);
         }
 
-        return process(drinks, a, b, 0, 0);
+        return bestTime1(drinks, a, b, 0, 0);
     }
 
-    private static int process(int[] drinks, int a, int b, int i, int i1) {
+    //问题二：所有杯子都干净的最短时间
 
-        return 0;
+    /**
+     *
+     * @param drinks    所有杯子可以开始洗的时间
+     * @param wash  单杯洗干净的时间
+     * @param air   挥发干净的时间（可以并行）
+     * @param index 当前做决定的杯子
+     * @param free  洗的机器什么时候可用
+     * @return  都变干净的最早时间
+     */
+    private static int bestTime1(int[] drinks, int wash, int air, int index, int free) {
+        if (index == drinks.length) {
+            return 0;
+        }
+        //index号杯子 决定洗
+        int selfClean1 = Math.max(drinks[index], free) + wash;
+        int restClean1 = bestTime1(drinks,wash,air,index+1,selfClean1);
+        int p1 = Math.max(selfClean1, restClean1);
+
+        //index号杯子 决定挥发
+        int selfClean2 = drinks[index] + air;
+        int restClean2 = bestTime1(drinks, wash, air, index + 1, free);
+        int p2 = Math.max(selfClean2, restClean2);
+
+        return Math.min(p1, p2);
     }
+
+    //动态规划版本
+    public static int bestTime2(int[] drinks, int wash, int air) {
+        int N = drinks.length;
+        int maxFree = 0;
+        for (int i = 0; i < drinks.length; i++) {
+            maxFree = Math.max(maxFree, drinks[i]) + wash;
+        }
+
+        int[][] dp = new int[N + 1][maxFree + 1];
+
+        //dp[N][...] = 0;
+
+        for (int index = N - 1; index >= 0; index--) {
+            for (int free = 0; free <= maxFree; free++) {
+
+                int selfClean1 = Math.max(drinks[index], free) + wash;
+                if (selfClean1 > maxFree) {
+                    continue;
+                }
+                int restClean1 = dp[index + 1][selfClean1];
+                int p1 = Math.max(selfClean1, restClean1);
+
+                //index号杯子 决定挥发
+                int selfClean2 = drinks[index] + air;
+                int restClean2 = dp[index + 1][free];
+                int p2 = Math.max(selfClean2, restClean2);
+
+                dp[index][free] = Math.min(p1, p2);
+            }
+        }
+
+        return dp[0][0];
+    }
+
 }
+
+
